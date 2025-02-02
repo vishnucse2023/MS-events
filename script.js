191a3c633620a0574be350236a3f1ffdc8faa49d
Vishnu Murthy M <121560191+vishnucse2023@users.noreply.github.com>
2025-02-02 13:10:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Form Submission Handling
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+    // You can add code here to send the form data to a server
+});
